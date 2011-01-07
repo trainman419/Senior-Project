35b04b7e72bbaabf6e7509b477534b682f263b6e
trainman419 <legotown@aol.com>
2011-01-06 22:19:24 -0800

    Porting the serial driver from the old source tree.

---
+++ arduino/serial.c
@@ -0,0 +1,189 @@
+/* serial.c
+ *
+ * Serial routines for ATmega32
+ * Modified to support the multiple UARTS of the Atmega2560
+ *
+ * Author: Austin Hendrix
+*/
+
+#include "serial.h"
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+// maybe increase size since the atmega2560 has more memory?
+
+/* recieve circular fifo (10 bytes total 20% overhead) */
+uint8_t rx_head[] = {0, 0, 0, 0}; /* points to next writeable byte */
+volatile uint8_t rx_size[] = {0, 0, 0, 0}; /* number of byts in buffer */
+uint8_t rx_buf[4][8];
+
+/* send circular fifo (10 bytes total, 20% overhead) */
+uint8_t tx_head[] = {0, 0, 0, 0}; /* next writeable byte */
+volatile uint8_t tx_size[] = {0, 0, 0, 0}; /* number of bytes in buffer */
+uint8_t tx_buf[4][8];
+
+/* recieve interrupt 0 */
+ISR(USART0_RX_vect) /* receive complete */
+{
+   /* read into fifo, allow overruns for now. */
+   rx_buf[0][rx_head[0]++] = UDR0;
+   rx_head[0] &= 7;
+   rx_size[0]++;
+}
+
+/* recieve interrupt 1 */
+ISR(USART1_RX_vect) /* receive complete */
+{
+   /* read into fifo, allow overruns for now. */
+   rx_buf[1][rx_head[1]++] = UDR1;
+   rx_head[1] &= 7;
+   rx_size[1]++;
+}
+
+/* recieve interrupt 2 */
+ISR(USART2_RX_vect) /* receive complete */
+{
+   /* read into fifo, allow overruns for now. */
+   rx_buf[2][rx_head[2]++] = UDR2;
+   rx_head[2] &= 7;
+   rx_size[2]++;
+}
+
+/* recieve interrupt 3 */
+ISR(USART3_RX_vect) /* receive complete */
+{
+   /* read into fifo, allow overruns for now. */
+   rx_buf[3][rx_head[3]++] = UDR3;
+   rx_head[3] &= 7;
+   rx_size[3]++;
+}
+
+/* determine if there is data in the rx buffer */
+uint8_t rx_ready(uint8_t port) {
+   return rx_size[port] > 0;
+}
+
+/* get a byte from recieve buffer. block until data recieved */
+uint8_t rx_byte(uint8_t port) {
+   while (!rx_size[port]);
+   return rx_buf[port][(rx_head[port] - rx_size[port]--) & 7];
+}
+
+/* transmit interrupt 0 */
+ISR(USART0_UDRE_vect) /* ready for more data to transmit */
+{
+   if (tx_size[0]) {
+      UDR0 = tx_buf[0][(tx_head[0] - tx_size[0]--) & 7];
+   } else {
+	   UCSR0B &= ~(1 << 5); /* disable send interrupt */
+   }
+}
+
+/* transmit interrupt 1 */
+ISR(USART1_UDRE_vect) /* ready for more data to transmit */
+{
+   if (tx_size[1]) {
+      UDR1 = tx_buf[1][(tx_head[1] - tx_size[1]--) & 7];
+   } else {
+	   UCSR1B &= ~(1 << 5); /* disable send interrupt */
+   }
+}
+
+/* transmit interrupt 2 */
+ISR(USART2_UDRE_vect) /* ready for more data to transmit */
+{
+   if (tx_size[2]) {
+      UDR2 = tx_buf[2][(tx_head[2] - tx_size[2]--) & 7];
+   } else {
+	   UCSR2B &= ~(1 << 5); /* disable send interrupt */
+   }
+}
+
+/* transmit interrupt 3 */
+ISR(USART3_UDRE_vect) /* ready for more data to transmit */
+{
+   if (tx_size[3]) {
+      UDR3 = tx_buf[3][(tx_head[3] - tx_size[3]--) & 7];
+   } else {
+	   UCSR3B &= ~(1 << 5); /* disable send interrupt */
+   }
+}
+
+/* determine if there is space for another byte in the transmit buffer */
+uint8_t tx_ready(uint8_t port) {
+   return tx_size[port] < 8;
+}
+
+volatile uint8_t * ucsr[] = {&UCSR0A, &UCSR1A, &UCSR2A, &UCSR3A};
+#define A 0
+#define B 1
+#define C 2
+
+/* put a byte in the transmit buffer. block until space available */
+void tx_byte(uint8_t port, uint8_t b) {
+   while (tx_size[port] > 7);
+
+   /* messing with buffer pointers is not atomic; need locking here */
+   ucsr[port][B] &= ~(1 << 5); /* diable send interrupt (just enough locking) */
+   tx_buf[port][tx_head[port]++] = b;
+   tx_head[port] &= 7;
+   tx_size[port]++;
+   /* done messing with buffer pointers */
+
+   ucsr[port][B] |= (1 << 5); /* enable send interrupt */
+}
+
+volatile uint8_t * rxtx[] = {&DDRE, &DDRD, &DDRH, &DDRJ};
+uint8_t rxbit[] = {0, 2, 0, 0};
+
+volatile uint16_t * ubrr[] = {&UBRR0, &UBRR1, &UBRR2, &UBRR3};
+
+/* setup and enable serial interrupts */
+void serial_init(uint8_t port)
+{
+   serial_stop(port);
+
+   /* rx pin setup */
+      /* set pin input */
+   rxtx[port][0] &= ~(1 << rxbit[port]);
+      /* enable input pin pull-up */
+   rxtx[port][1] &= ~(1 << rxbit[port]);
+
+   /* tx pin setup: set pin as output */
+   rxtx[port][0] |= (1 << (rxbit[port]+1));
+
+	/* USART init code */
+   ucsr[port][B] = 0x18; /* RX and TX enable, interrupts */
+	ucsr[port][C] = 0x8E; /* no parity, 1 stop, 8bit */
+
+   // TODO: abstract this out
+	//UBRRH = 0x00; /* high byte, serial divisor */
+   //UBRRL = 0x67; /* low byte, serial divisor. 9600 baud */
+   //UBRRL = 0x63; /* low byte, serial divisor. 10000 baud */
+   *ubrr[port] = 0x67; /* serial divisor 9600 baud */
+
+   /* buffer init */
+   tx_head[port] = 0;
+   tx_size[port] = 0;
+   rx_head[port] = 0;
+   rx_size[port] = 0;
+   
+
+	/* set the USART_RXC interrupt enable bit */
+   ucsr[port][B] |= (1 << 7);
+}
+
+void serial_baud(uint8_t port, uint16_t baud) {
+   uint32_t ubr = 1000000;
+   ubr /= baud;
+   // FIXME: deal with baud rates that are too high here
+   *ubrr[port] = ubr;
+}
+
+/* stops the serial interrupts */
+void serial_stop(uint8_t port)
+{
+	/* clear both interrupt enable bits */
+   ucsr[port][B] &= ~( (1 << 5) | (1 << 7));
+}
+
---
+++ arduino/serial.h
@@ -0,0 +1,31 @@
+/* serial.h
+ *
+ * header file for serial function for ATMega32
+ *
+ * Author: Austin Hendrix
+ */
+
+#ifndef SERIAL_H
+#define SERIAL_H
+
+#include <avr/io.h>
+
+/* setup and enable serial interrupts */
+void serial_init(uint8_t port);
+
+/* stops the serial interrupts */
+void serial_stop(uint8_t port);
+
+/* put a byte in the transmit buffer. block until space available */
+void tx_byte(uint8_t port, uint8_t b);
+
+/* determine if there is space for another byte in the transmit buffer */
+uint8_t tx_ready(uint8_t port);
+
+/* determine if there is data in the rx buffer */
+uint8_t rx_ready(uint8_t port);
+
+/* get a byte from recieve buffer. block until data recieved */
+uint8_t rx_byte(uint8_t port);
+
+#endif
