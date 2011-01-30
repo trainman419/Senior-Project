92048018fdc0d172293df7987ea99c4766e94fc0
Austin Hendrix <hendrix@pcp066652pcs.wireless.calpoly.edu>
2011-01-30 00:31:29 -0800

    Oops, missed an include.

---
+++ arduino/main.h
@@ -0,0 +1,18 @@
+/* main.h
+ *
+ * Common variables and constants
+ *
+ * Author: Austin Hendrix
+ */
+
+#ifndef MAIN_H
+#define MAIN_H
+
+#define BRAIN 0
+#define BT 3
+#define GPS 2
+#define SONAR 1
+
+extern volatile uint16_t shutdown_count;
+
+#endif
