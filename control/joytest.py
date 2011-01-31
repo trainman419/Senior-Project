289f1c57b6429dd96774a16a8974bdeac7376b15
trainman419 <legotown@aol.com>
2011-01-30 17:17:44 -0800

    Add joystick test program.

---
+++ control/joytest.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+
+import time
+import pygame
+
+pygame.init()
+
+pygame.joystick.init()
+
+print "Detected " + str(pygame.joystick.get_count()) + " joysticks"
+
+if pygame.joystick.get_count > 0:
+   joystick = pygame.joystick.Joystick(0)
+   joystick.init()
+
+   print "Joystick " + joystick.get_name()
+   numaxes = joystick.get_numaxes()
+   print "Joystick has " + str(numaxes) + " axes"
+   
+
+   while True:
+      time.sleep(1)
+      pygame.event.pump()
+      for i in range(numaxes):
+         print "Axis " + str(i) + ": " + str(joystick.get_axis(i))
