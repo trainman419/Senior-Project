0258ad52389d11e7478bf93724ca17f76cb87d47
trainman419 <legotown@aol.com>
2011-02-12 12:59:22 -0800

    Add protocol libraries.

---
+++ protocol/protocol.cpp
@@ -0,0 +1,8 @@
+/* protocol.cpp
+ *
+ * A cross-platform communication library for AVR and x86.
+ *
+ * Author: Austin Hendrix
+ */
+
+#include "protocol.h"
---
+++ protocol/protocol.h
@@ -0,0 +1,9 @@
+/* protocol.h
+ *
+ * A cross-platofrm AVR and x86 serial communication library.
+ *
+ * Author: Austin Hendrix
+ */
+
+class Packet {
+};
