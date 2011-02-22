cdbdb4334bdafe57ddb127fcafa52c1e94df3074
Austin Hendrix <hendrix@elwood.local>
2011-02-21 21:34:33 -0800

    Predict step of kalman filter.

--- ros/gps_odometry/src/gps_odometry.cpp
+++ ros/gps_odometry/src/gps_odometry.cpp
@@ -16,6 +16,8 @@
 matrix<1, 3> position(0.0);
 matrix<3, 3> covariance(0.0);
 
+matrix<3, 3> gain; // kalman filter gain
+
 // receive a GPS update
 void gpsCallback(const gps_common::GPSFix::ConstPtr &gps) {
    ROS_INFO("Got gps message");
@@ -27,18 +29,49 @@ void gpsCallback(const gps_common::GPSFix::ConstPtr &gps) {
 void odometryCallback(const nav_msgs::Odometry::ConstPtr &odo) {
    ROS_INFO("Got odometry message");
 
-   // Kalman filter prediction step
+   matrix<1, 3> update;
 
-   // FIXME: mostly garbage code to test that the matrix class compiles
-   matrix<1, 3> update(0.0);
+   update.data[0][0] = odo->pose.pose.position.x; // x
+   update.data[0][1] = odo->pose.pose.position.y; // y
+   update.data[0][2] = odo->pose.pose.orientation.x; // rot TODO: pick units/normalize
+
+   matrix<3, 3> Rt; // covariance of update noise
+
+   /* odo->pose->covariance
+      Row-major representation of the 6x6 covariance matrix
+      The orientation parameters use a fixed-axis representation.
+      In order, the parameters are:
+      (x, y, z, rotation about X axis, rotation about Y axis, rotation about Z axis)
+      */
+   // x/y pairings
+   Rt.data[0][0] = odo->pose.covariance[0 + 6*0]; // x x
+   Rt.data[0][1] = odo->pose.covariance[0 + 6*1]; // x y
+   Rt.data[1][0] = odo->pose.covariance[1 + 6*0]; // y x
+   Rt.data[1][1] = odo->pose.covariance[1 + 6*1]; // y y
+
+   Rt.data[0][2] = odo->pose.covariance[0 + 6*3]; //  x  rot
+   Rt.data[1][2] = odo->pose.covariance[1 + 6*3]; //  y  rot
+   Rt.data[2][0] = odo->pose.covariance[3 + 6*0]; // rot  x
+   Rt.data[2][1] = odo->pose.covariance[3 + 6*1]; // rot  y
+   Rt.data[2][2] = odo->pose.covariance[3 + 6*3]; // rot rot
+
+   // Kalman filter prediction step (inputs/odometry)
+   // A and B are identity matrices
    position = update + position;
-
-   update = position * covariance;
-   covariance = covariance.T();
-   covariance = invert(covariance);
+   covariance = covariance + Rt;
 }
 
 int main(int argc, char ** argv) {
+   // set up kalman gains
+   gain.data[0][0] = 1.0;
+   gain.data[0][1] = 1.0;
+   gain.data[0][2] = 1.0;
+   gain.data[1][0] = 1.0;
+   gain.data[1][1] = 1.0;
+   gain.data[1][2] = 1.0;
+   gain.data[2][0] = 1.0;
+   gain.data[2][1] = 1.0;
+   gain.data[2][2] = 1.0;
 
    ros::init(argc, argv, "gps_odometry");
 
--- ros/gps_odometry/src/matrix_test.cpp
+++ ros/gps_odometry/src/matrix_test.cpp
@@ -14,10 +14,30 @@ int main(int argc, char ** argv) {
    matrix<3, 3> a = I<3>();
    matrix<3, 3> b = I<3>();
    matrix<3, 3> c = a + b;
+   matrix<3, 3> d;
+
+   d.data[0][0] = 1.0;
+   d.data[0][1] = 2.0;
+   d.data[0][2] = 3.0;
+   d.data[1][0] = 4.0;
+   d.data[1][1] = 1.0;
+   d.data[1][2] = 2.0;
+   d.data[2][0] = 3.0;
+   d.data[2][1] = 4.0;
+   d.data[2][2] = 1.0;
 
    cout << a << endl;
    cout << a.T() << endl;
    cout << c << endl;
    cout << invert(c) << endl;
    cout << invert(c) * c << endl;
+   cout << d << endl;
+
+   cout << invert(d) << endl;
+   /* ought to be:
+    * [ [ 1, 0, 0]
+    *   [ 0.571, -0.142, 0]
+    *   [ 0.361, 0.056, -0.194] ]
+    */
+   cout << invert(d) * d << endl;
 }
