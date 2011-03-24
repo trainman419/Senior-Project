8f907755e6e52da881bf2accbf38004277e0b378
trainman419 <legotown@aol.com>
2011-03-23 19:32:56 -0700

    Adding Android project.

---
+++ android/src/com/namniart/RobotControl.java
@@ -0,0 +1,13 @@
+package com.namniart;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class RobotControl extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
