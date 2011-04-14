4e4e0295cf8502c272d7ce57938117caddfd3bf5
trainman419 <legotown@aol.com>
2011-04-14 12:31:36 -0700

    Update goal list; base case works.

--- ros/goal_list/src/goal_list.cpp
+++ ros/goal_list/src/goal_list.cpp
@@ -14,61 +14,86 @@
 #include <global_map/Location.h>
 #include <global_map/Offset.h>
 
-// declare ourselves to be "at" a goal when we are within 5 meters
-#define GOAL_DIST 5.0
+#include <goal_list/Goal.h>
+#include <goal_list/GoalList.h>
 
-// GPS coordinate at microdgrees latitude and longitude
-struct GPS {
-   int32_t lat;
-   int32_t lon;
-};
+#include <nav_msgs/Odometry.h>
 
-std::vector<GPS> * goals;
+// declare ourselves to be "at" a goal when we are within 1 meter
+#define GOAL_DIST 10.0
+
+using namespace std;
+
+vector<global_map::Location> * goals;
 unsigned int current_goal;
-global_map::Location current_loc;
-
-// client for resolving lat/lon to offsets
-ros::ServiceClient o_client;
 
 // publisher for current goal
 ros::Publisher goal_pub;
    
-void positionCallback(const global_map::Location::ConstPtr & msg) {
-   double dist = hypot(msg->row - current_loc.row, 
-                        msg->col - current_loc.col);
+void positionCallback(const nav_msgs::Odometry::ConstPtr & msg) {
+   ROS_INFO("Got position update");
 
-   if( dist < GOAL_DIST ) {
-      // advance to next goal
-      current_goal++;
-      if( current_goal >= goals->size() ) {
-         // umm... do we go back to goal #1 or just stop here?
-         // going back to the start is easier to handle
-         current_goal = 0;
-      } 
-      ROS_INFO("Advancing to goal %d", current_goal);
-      global_map::Offset offset;
-      offset.request.lat = goals->at(current_goal).lat / 1000000.0;
-      offset.request.lon = goals->at(current_goal).lon / 1000000.0;
-      if( o_client.call(offset) ) {
-         current_loc = offset.response.loc;
-         // broadcast this as the robot's new goal.
-         goal_pub.publish(current_loc);
-      } else {
-         ROS_ERROR("Failed to resolve offset");
+   if( current_goal < goals->size() ) {
+      double dist = hypot(msg->pose.pose.position.y - 
+                              goals->at(current_goal).row, 
+                          msg->pose.pose.position.x - 
+                              goals->at(current_goal).col);
+
+      if( dist < GOAL_DIST ) {
+         // advance to next goal
+         current_goal++;
+         goal_list::Goal g;
+         if( current_goal >= goals->size() ) {
+            // umm... do we go back to goal #1 or just stop here?
+            // going back to the start is easier to handle
+            current_goal = goals->size();
+            g.valid = 0;
+         } else {
+            ROS_INFO("Advancing to goal %d", current_goal);
+            g.loc = goals->at(current_goal);
+            g.valid = 1;
+         }
+         goal_pub.publish(g);
       }
    }
 }
 
+// TODO: rewrite this so that we don't have to have a thousand special cases
+//  most likely this means implementing full-duplex goal list exchange with
+//  the UI, and letting it determine what the next goal is
+void goalListCallback(const goal_list::GoalList::ConstPtr & msg) {
+   ROS_INFO("Got Goal List, length %d", msg->goals.size());
+   int oldsize = goals->size();
+   *goals = msg->goals;
+   if( goals->size() == 0) {
+      goal_list::Goal g;
+      g.valid = 0;
+      goal_pub.publish(g);
+   } else if( goals->size() < current_goal || oldsize == 0 ) {
+      current_goal = 0;
+      goal_list::Goal g;
+      g.valid = 1;
+      g.loc = goals->at(current_goal);
+      goal_pub.publish(g);
+   } else if( goals->size() > current_goal && current_goal == oldsize ) {
+      goal_list::Goal g;
+      g.valid = 1;
+      g.loc = goals->at(current_goal);
+      goal_pub.publish(g);
+   }
+
+}
+
 int main(int argc, char ** argv) {
+   goals = new vector<global_map::Location>();
+
    ros::init(argc, argv, "goal_list");
 
    ros::NodeHandle n;
 
-   o_client = n.serviceClient<global_map::Offset>("Offset");
-
-
    ros::Subscriber position = n.subscribe("position", 2, positionCallback);
-   goal_pub = n.advertise<global_map::Location>("current_goal", 10);
+   ros::Subscriber goalList = n.subscribe("goal_list", 2, goalListCallback);
+   goal_pub = n.advertise<goal_list::Goal>("current_goal", 10);
 
    ros::spin();
 }
