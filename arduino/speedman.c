838e1d38b8e874d8c50836ce0f7da86549fdc4cf
trainman419 <legotown@aol.com>
2011-04-17 23:21:03 -0700

    Updated speedman, better control but buggy.

--- arduino/speedman.c
+++ arduino/speedman.c
@@ -25,33 +25,32 @@
 #include "speedman.h"
 #include "wheelmon.h"
 #include "motor.h"
+#include "bump.h"
 
 #include <stdio.h>
 #include "main.h"
 
+#include <math.h>
+
 #define DIV 256
 
 #define abs(a) ((a)>0?(a):-(a))
-
-// speed controller mode
-uint8_t mode;
 
 volatile int16_t power = 0; 
 volatile int16_t target_speed;
 
 void speedman() {
    int16_t speed = 0;
-   uint16_t mult = DIV;
-   // keep track of what the speed control thinks we're doing
-   //s08 dir = 0; // 0: stopped 1: forward -1: reverse
-   mode = M_OFF;
+   //uint16_t mult = DIV;
+   double mult = DIV;
 
    // true PID control:
    // e: error
    // MV = Kp*e + Ki*integral(e, 0 to t) + Kd*de/dt
    int16_t e = 0; // error
 
-   const static int16_t Kp = 1; // proportional constant
+   //const static int16_t Kp = 1; // proportional constant
+   const static double Kp = DIV/16; // proportional constant
 
    schedule(100); // 10 times/second
    //schedule(200); // 5 times/second
@@ -60,21 +59,28 @@ void speedman() {
       speed = qspeed;
 
       e = target_speed - speed; 
-      if( target_speed < 0 ) e = -e;
+      //if( target_speed < 0 ) e = -e;
 
-      mult += e * Kp;
+      //mult += e * Kp;
+      if( target_speed != 0 ) {
+         mult += Kp * (((double)e) / ((double)target_speed));
+         //mult += Kp * e;
+      }
 
       if( mult < 1 ) mult = 1;
-      if( mult > DIV ) mult = DIV;
+      if( fabs(mult*target_speed) > 100.0*DIV ) 
+         mult = 100.0*(double)DIV/(double)target_speed;
 
-      power = mult * target_speed;
+      power = mult * (double)target_speed;
 
       // hardcoded full stop
-      if( target_speed == 0 && speed == 0 ) power = 0;
+      //if( target_speed == 0 && speed == 0 ) power = 0;
 
       // power limits
-      if( power/DIV > 100 ) power = DIV*100;
-      if( power/DIV < -100 ) power = -DIV*100;
+      //if( power/DIV > 100 ) power = DIV*100;
+      //if( power/DIV < -100 ) power = -DIV*100;
+
+      if( power > 0 && bump() ) power = 0;
 
       // output
       motor_speed(power/DIV);
--- arduino/speedman.h
+++ arduino/speedman.h
@@ -9,13 +9,6 @@
 #ifndef SPEEDMAN_H
 #define SPEEDMAN_h
 
-// speed controller mode
-#define M_OFF 0
-#define M_FORWARD 1
-#define M_BRAKE 2
-#define M_REVERSE 3
-extern uint8_t mode;
-
 extern volatile int16_t power;
 extern volatile int16_t target_speed;
 
