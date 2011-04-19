8bd139794c18ab0d2b14070892f362fc5d55b5a0
trainman419 <legotown@aol.com>
2011-04-18 22:56:57 -0700

    Add local obstacle map and obstacle avoidance to path planner.

--- ros/path_planner/src/path_planner.cpp
+++ ros/path_planner/src/path_planner.cpp
@@ -35,6 +35,7 @@
 #include <hardware_interface/Control.h>
 #include <goal_list/Goal.h>
 #include <nav_msgs/Odometry.h>
+#include <sensor_msgs/LaserScan.h>
 
 using namespace std;
 
@@ -76,8 +77,74 @@ struct loc {
 // our current path
 list<loc> * path;
 
+// the local obstacle map
+// fixed dimensions: 10m by 10m, centered about the associated center point
+// FIXME: replace this with calls to the global_map and SLAM
+int map_data[101][101];
+double map_center_x = 0.0;
+double map_center_y = 0.0;
+
+// get the value of the local obstacle map at (x, y)
+//  return 0 for any point not within the obstacle map
+inline int map_get(double x, double y) {
+   int i = round(x - map_center_x) + 50;
+   int j = round(y - map_center_y) + 50;
+   if( i >= 0 && i < 101 && j >= 0 && j < 101 ) {
+      return map_data[i][j];
+   } else {
+      return 0;
+   }
+}
+
+// get the value of the local obstacle map at (x, y)
+//  return 0 for any point not within the obstacle map
+inline void map_set(double x, double y, int v) {
+   int i = round(x - map_center_x) + 50;
+   int j = round(y - map_center_y) + 50;
+   if( i >= 0 && i < 101 && j >= 0 && j < 101 ) {
+      map_data[i][j] = v;
+   }
+}
+
+   struct corner {
+      int x;
+      int y;
+   };
+   int comp(const void * c, const void * d) {
+      const corner * a = (const corner*)c;
+      const corner * b = (const corner*)d;
+      if( a->x < b->x ) return -1;
+      if( a->x > b->x ) return 1;
+      if( a->y < b->y ) return -1;
+      if( a->y > b->y ) return 1;
+      return 0;
+   }
+
 // test if we have a collision at a particular point
 bool test_collision(loc here) {
+   // robot dimensions: width: 4; length: 7
+   // laser 2.5 back from front, at logical center of robot
+   // Base corners
+   //  3-------0
+   //  |   ->  |
+   //  2-------1
+   double c_x[] = {2.5, 2.5, -4.5, -4.5};
+   double c_y[] = {2.0, -2.0, 2.0, -2.0};
+
+   // translate corners onto fixed coordinate system
+   corner c[4];
+   for( int i=0; i<4; i++ ) {
+      c[i].x = round(here.x + c_x[i]*cos(here.pose) - c_y[i]*sin(here.pose));
+      c[i].y = round(here.y + c_y[i]*cos(here.pose) + c_x[i]*sin(here.pose));
+   }
+
+   qsort(c, sizeof(corner), 4, comp);
+
+   cout << "Corners: " << endl;
+   for( int i=0; i<4; i++ ) {
+      cout << "(" << c[i].x << ", " << c[i].y << ")" << endl;
+   }
+
    // hack; for now, ignore obstacles and just plan a path
    return false;
 }
@@ -172,10 +239,12 @@ void plan_path(loc start, loc end) {
 
          while( n.pose < -M_PI ) n.pose += M_PI*2;
          while( n.pose >  M_PI ) n.pose -= M_PI*2;
-         points->push_back(n);
+         if( !test_collision(n) ) {
+            points->push_back(n);
 
-         double len = dist(n, end);
-         (*unvisited)[len] = n.idx;
+            double len = dist(n, end);
+            (*unvisited)[len] = n.idx;
+         }
       }
 
       // visit the point nearest the goal
@@ -214,7 +283,6 @@ bool path_valid = false;
 loc goal;
 
 void goalCallback(const goal_list::Goal::ConstPtr & msg) {
-   // TODO: do stuff when we update the goal
    if( msg->valid ) {
       goal.x = msg->loc.col;
       goal.y = msg->loc.row;
@@ -227,14 +295,16 @@ void goalCallback(const goal_list::Goal::ConstPtr & msg) {
    }
 }
 
-// TODO: update this to take one of the stock messages that better represents
-// position, with error estimates
+// the last location we were at.
+//  used as the center point for our local map
+loc last_loc;
+   
 void positionCallback(const nav_msgs::Odometry::ConstPtr & msg) {
-   // TODO: do stuff when our position changes
    loc here;
    here.x = msg->pose.pose.position.x;
    here.y = msg->pose.pose.position.y;
    here.pose = (M_PI/2) - msg->pose.pose.orientation.x;
+   last_loc = here;
    if( active ) {
       // if our path is invalid due to a new goal or such, re-plan
       if( !path_valid ) {
@@ -286,6 +356,32 @@ void positionCallback(const nav_msgs::Odometry::ConstPtr & msg) {
    }
 }
 
+void laserCallback(const sensor_msgs::LaserScan::ConstPtr & msg) {
+   map_center_x = last_loc.x;
+   map_center_y = last_loc.y;
+
+   double theta_base = last_loc.pose;
+
+   double theta = theta_base + msg->angle_min;
+   double x;
+   double y;
+
+   for( int i=0; i<101; i++ ) {
+      for( int j=0; j<101; j++ ) {
+         map_data[i][j] = 0;
+      }
+   }
+
+   for( unsigned int i=0; i<msg->ranges.size(); i++, 
+         theta += msg->angle_increment ) {
+      x = map_center_x + msg->ranges[i]*cos(theta);
+      y = map_center_y + msg->ranges[i]*sin(theta);
+      map_set(x, y, 1);
+   }
+
+   return;
+}
+
 int main(int argc, char ** argv) {
    path = new list<loc>();
 
@@ -296,6 +392,7 @@ int main(int argc, char ** argv) {
    // subscribe to our location and current goal
    ros::Subscriber pos_sub = n.subscribe("position", 10, positionCallback);
    ros::Subscriber goal_sub = n.subscribe("current_goal", 10, goalCallback);
+   ros::Subscriber laser_sub = n.subscribe("scan", 1, laserCallback);
 
    move_pub = n.advertise<path_planner::Move>("move_commands", 10);
    control_pub = n.advertise<hardware_interface::Control>("control", 10);
