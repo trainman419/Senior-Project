06f0fa7bb0ea34dfeb0b85cbb2786e43895ac577
trainman419 <legotown@aol.com>
2011-06-03 02:59:42 -0700

    Update line counter

--- lines.sh
+++ lines.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-find . \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.java' \) -a \! \( -wholename '*CMakeFiles*' -o -wholename '*cpp/include*' \) | xargs wc -l
+find . \( -name '*.c' -o -name '*.cpp' -o -name '*.h' -o -name '*.java' -o -name '*.py' -o -name '*.pl' \) -a \! \( -wholename '*CMakeFiles*' -o -wholename '*cpp/include*' -o -wholename '*/_*' \) | xargs wc -l
