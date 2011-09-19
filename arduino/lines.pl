7d07067d2224c928d25a797f37723264c0e51348
trainman419 <legotown@aol.com>
2011-09-18 23:08:17 -0700

    Add line counter.

---
+++ arduino/lines.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+
+use strict;
+
+my $comment = 0;
+my $file = $ARGV;
+
+my $type;
+my $count = 0;
+my $total = 0;
+
+sub type() {
+   $file =~ m/^.*?\.([^\.]+)$/;
+   $type = $1;
+}
+
+while(<>) {
+   if($file ne $ARGV) {
+      if( length $file ) {
+         print "$file: " if $file ne "-";
+         print "$count countable lines\n";
+      }
+      $total += $count;
+
+      $count = 0;
+      $comment = 0;
+      $file = $ARGV;
+      type();
+   }
+
+   if( $type eq "c" or $type eq "cpp" or $type eq "h" ) {
+      # count C/C++ lines
+      
+      # strip block comments
+      s,/\*.*?\*/,,g;
+      m,\*/, and $comment = 0;
+      m,/\*, and $comment = 1;
+
+      if(not $comment) {
+         s,//.*,,;
+         s,\s+,,g;
+         s,^}$,,;
+         s,^};$,,;
+         s,^{$,,;
+         $count++ if length;
+      }
+   } elsif( $type eq "S" ) {
+      # count assembly lines
+      s,;.*,,;
+      s,\s+,,g;
+      $count++ if length;
+   }
+}
+
+$total += $count;
+
+print "$total total lines\n";
