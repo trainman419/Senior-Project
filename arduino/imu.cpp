50c05dfcfbc85683693fd41212d73ac5a96482f1
Austin Hendrix <legotown@aol.com>
2012-01-09 22:20:10 -0800

    Add filtering to IMU.

--- arduino/imu.cpp
+++ arduino/imu.cpp
@@ -41,31 +41,20 @@ void imu_init() {
    i2c_init();
    // set up accelerometer
    i2c_write(I2C_ACCEL, 0x2A, 0x00); // disable tap detection
-   i2c_wait();
    i2c_write(I2C_ACCEL, 0x2C, 0x09); // 50Hz data rate
-   i2c_wait();
    i2c_write(I2C_ACCEL, 0x31, 0x0B); // full res mode & +/- 16g range
-   i2c_wait();
    i2c_write(I2C_ACCEL, 0x38, 0x00); // disable FIFO; always get recent sample
-   i2c_wait();
    i2c_write(I2C_ACCEL, 0x2D, 0x08); // enable measurement mode
-   i2c_wait();
 
    // set up compass
    i2c_write(I2C_COMPASS, 0x00, 0x18); // 50Hz mode
-   i2c_wait();
    i2c_write(I2C_COMPASS, 0x01, 0x20); // gain: 1300 counts/milli-gauss
-   i2c_wait();
    i2c_write(I2C_COMPASS, 0x02, 0x00); // continuous-conversion mode
-   i2c_wait();
 
    // set up gyro
    i2c_write(I2C_GYRO, 0x15, 19); // 50Hz sample rate
-   i2c_wait();
    i2c_write(I2C_GYRO, 0x16, 0x1C); // 20Hz low-pass filter
-   i2c_wait();
    i2c_write(I2C_GYRO, 0x3E, 0x01); // X gyro as clock reference
-   i2c_wait();
 }
 
 uint8_t common_buf[8];
@@ -124,14 +113,14 @@ void gyro_read() {
 void compass_done(uint8_t * buf) {
    // interpret and publish data
    int16_t compass = (buf[0] << 8) | buf[1];
-   compass_msg.x = compass/1300.0;
+   compass_msg.x = ((compass/1300.0) + compass_msg.x) / 2;
 
    compass = (buf[2] << 8) | buf[3];
-   compass_msg.y = compass/1300.0;
+   compass_msg.y = ((compass/1300.0) + compass_msg.y) / 2;
 
    compass = (buf[4] << 8) | buf[5];
-   compass_msg.z = compass/1300.0;
-//   compass_pub.publish(&compass_msg);
+   compass_msg.z = ((compass/1300.0) + compass_msg.z) / 2;
+   compass_pub.publish(&compass_msg);
 
    gyro_read();
 }
@@ -145,14 +134,14 @@ void accel_done(uint8_t * buf) {
    // interpret and publish data
    int16_t accel;
    accel = buf[0] | (buf[1] << 8);
-   accel_msg.x = accel / 256.0;
+   accel_msg.x = ((accel / 256.0) + 3*accel_msg.x) / 4;
 
    accel = buf[2] | (buf[3] << 8);
-   accel_msg.y = accel / 256.0;
+   accel_msg.y = ((accel / 256.0) + 3*accel_msg.y) / 4;
 
    accel = buf[4] | (buf[5] << 8);
-   accel_msg.z = accel / 256.0;
-//   accel_pub.publish(&accel_msg);
+   accel_msg.z = ((accel / 256.0) + 3*accel_msg.z) / 4;
+   accel_pub.publish(&accel_msg);
 
    compass_read();
 }
